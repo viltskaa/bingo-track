67a20ac4e7c57034b2b12bf18ea08cc8e550276b
Viltskaa <andrewstyou@gmail.com>
2023-03-07 23:16:07 +0400

    add frontend

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React, {createContext} from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './Styles/app.css'
-import FireAPI from "./ServerData/FireBase.js";
-
-export const firebaseContext = createContext(new FireAPI());
-
-ReactDOM.createRoot(document.getElementById('root')).render((<App />))
